7cf065bc6fcbc363da67209baa49c2a5e8831f67
thanhdangg <102210310@sv1.dut.udn.vn>
2024-09-14 15:33:55 +0700

    add train

--- Task2_Lesion_Attribute_Detection/image_process.py
+++ Task2_Lesion_Attribute_Detection/image_process.py
@@ -2,7 +2,7 @@ import argparse
 import cv2
 import numpy as np
 import os
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
 
 
 def normalize_image(image):
@@ -16,38 +16,44 @@ def augment_image(image):
     rotated_image = cv2.rotate(flipped_image, cv2.ROTATE_90_CLOCKWISE)
     return rotated_image
 
-def process_image(input_path, output_path, size):
+def process_image(input_path, size):
     image = cv2.imread(input_path)
+    if image is None:
+        raise ValueError(f"Could not read image at {input_path}")
     
     image = normalize_image(image)
-    
     image = resize_image(image, size)
+    image = augment_image(image)    
+    return image
     
-    image = augment_image(image)
-    
-    cv2.imwrite(output_path, image * 255)
-
-    
-def process_images_in_folder(input_folder, output_folder, size):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+def process_images_in_folder(input_folder, size):   
+    images = []
+    filenames = [] 
     
     for filename in os.listdir(input_folder):
         input_path = os.path.join(input_folder, filename)
-        output_path = os.path.join(output_folder, filename)
-        process_image(input_path, output_path, size)
+        try:
+            image = process_image(input_path, size)
+            images.append(image)
+            filenames.append(filename)
+        except ValueError as e:
+            print(e)
+    
+    images = np.array(images)
+    return images, filenames
+
 
 def main():
     parser = argparse.ArgumentParser(description="Process and augment images.")
     arg = parser.add_argument
 
     arg("--input_folder", type=str,default ="./data/ISIC2018_Task1-2_Training_Input", help="Path to the input folder containing images.")
-    arg("--output_folder", type=str,default="./data/image_processed", help="Path to save the processed images.")
     arg("--size", type=int, nargs=2, default=(256, 256), help="Size to resize the images to (width height).")
     
     args = parser.parse_args()
     
-    process_images_in_folder(args.input_folder, args.output_folder, tuple(args.size)) 
+    images, filenames = process_images_in_folder(args.input_folder, tuple(args.size))
+    print(f"Processed {len(images)} images.")
 
 if __name__ == "__main__":
     main()
